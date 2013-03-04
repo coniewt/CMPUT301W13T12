4ea70b18ea2a32e7f2a583aa8afe79541f20d9af
Yuwe DUan <yuwei@ualberta.ca>
2013-03-03 18:42:21 -0700

    update

---
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/model/ElasticSearchResponse.java
@@ -0,0 +1,14 @@
+package ca.ualberta.c301w13t12recipes.model;
+
+public class ElasticSearchResponse<T> {
+    String _index;
+    String _type;
+    String _id;
+    int _version;
+    boolean exists;
+    T _source;
+    double max_score;
+    public T getSource() {
+        return _source;
+    }
+}
