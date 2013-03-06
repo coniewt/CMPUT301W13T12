4350af63a1b964b201f4541401b4f8a89536dfc9
Yuwe DUan <yuwei@ualberta.ca>
2013-03-05 17:35:17 -0700

    update

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/model/ElasticSearchResponse.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/model/ElasticSearchResponse.java
@@ -1,14 +1,26 @@
 package ca.ualberta.c301w13t12recipes.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+
 public class ElasticSearchResponse<T> {
-    String _index;
-    String _type;
-    String _id;
-    int _version;
-    boolean exists;
-    T _source;
-    double max_score;
-    public T getSource() {
-        return _source;
+	int took;
+    boolean timed_out;
+    transient Object _shards;
+    Hits<T> hits;
+    boolean exists;    
+    public Collection<ElasticSearchResponse<T>> getHits() {
+        return hits.getHits();        
+    }
+    public Collection<T> getSources() {
+        Collection<T> out = new ArrayList<T>();
+        for (ElasticSearchResponse<T> essrt : getHits()) {
+            out.add( essrt.getSource() );
+        }
+        return out;
+    }
+    public String toString() {
+        return (super.toString() + ":" + took + "," + _shards + "," + exists + ","  + hits);     
     }
 }
