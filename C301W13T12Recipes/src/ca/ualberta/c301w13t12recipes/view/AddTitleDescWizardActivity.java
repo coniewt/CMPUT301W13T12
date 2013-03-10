a2f6c4c01038e123e073d61d97cef8d49c6f7cd3
Frank <guanqi@ualberta.ca>
2013-03-10 13:30:36 -0600

    Updates: 1.update the id for the widgets in activity_add_ingred_wizard.xml 2.initialize next and cancel button for AddTitleDescWizardActivity
    By guanqi

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddTitleDescWizardActivity.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddTitleDescWizardActivity.java
@@ -1,17 +1,47 @@
 package ca.ualberta.c301w13t12recipes.view;
 
 import ca.ualberta.c301w13t12recipes.R;
+import ca.ualberta.c301w13t12recipes.model.Recipe;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddTitleDescWizardActivity extends Activity {
-
+	
+	private EditText descEditText; //description widget
+	private EditText nameEditText; //name widget
+	
+	private Button cancelButton; // cancel button
+	private Button nextButton;// next button
+	
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    this.setContentView(R.layout.activity_add_title_desc_wizard);
+	    
+	    setupButton();// Initialize all the buttons
+	    setupEditText();//Initialize all the EditText widgets
+	    
+	    
+	    
+	    
 	    // TODO Auto-generated method stub
 	}
-
+	private void setupButton(){
+		cancelButton = (Button)findViewById(R.id.add_step1_cancel_button);
+		nextButton = (Button)findViewById(R.id.add_step1_next_button);
+		
+	}
+	private void setupEditText(){
+		descEditText =(EditText)findViewById(R.id.add_editText_description);
+		nameEditText =(EditText)findViewById(R.id.add_editText_recipe_name);
+		
+	}
+	private Recipe createRecipe(){
+		return new Recipe(null,null,null,null,null);
+	}
+	
 }
