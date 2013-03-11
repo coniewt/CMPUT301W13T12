eb2cb8fc2b28afe3a6087825650feb4f468aa003
Frank <guanqi@ualberta.ca>
2013-03-11 07:15:08 -0600

    almost die

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddIngredWizardActivity.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddIngredWizardActivity.java
@@ -10,8 +10,12 @@ import ca.ualberta.c301w13t12recipes.controller.DatabaseController;
 import ca.ualberta.c301w13t12recipes.controller.IngredientsAdapter;
 import ca.ualberta.c301w13t12recipes.model.Recipe;
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
@@ -24,10 +28,27 @@ public class AddIngredWizardActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_add_ingred_wizard);
+	    
 	    ListView lv = (ListView)findViewById(R.id.listView_ingredients_list);
+	    
 	    Recipe recipe =(Recipe)getIntent().getSerializableExtra("NEW_RECIPE");
 	    IngredientsAdapter adapter = new IngredientsAdapter();
 	    lv.setAdapter(adapter.getAdapter(this,recipe.getIngredients()));
+	    
+	    lv.setOnClickListener( new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Cursor cursor = (Cursor)lv.getItemAtPosition(position);
+				
+				
+				
+				
+			}
+	    	
+	    });
+	    
 	    
 	    // TODO Auto-generated method stub
 	}
