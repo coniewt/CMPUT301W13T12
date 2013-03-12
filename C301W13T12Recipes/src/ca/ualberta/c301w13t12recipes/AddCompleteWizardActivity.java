8eaad4db0d2e8c7f8a5da9b587f22f6b3ce358b8
Frank <guanqi@ualberta.ca>
2013-03-11 21:04:01 -0600

    add AddCompleteWizardActivity

---
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/AddCompleteWizardActivity.java
@@ -0,0 +1,18 @@
+package ca.ualberta.c301w13t12recipes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import ca.ualberta.c301w13t12recipes.R;
+
+public class AddCompleteWizardActivity extends Activity {
+	Button doneButton;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_add_complete_wizard);
+	    // TODO Auto-generated method stub
+	}
+
+}
