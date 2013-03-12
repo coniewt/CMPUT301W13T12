94be45772898dd469823cfb2d35a08ca5044bfee
Frank <guanqi@ualberta.ca>
2013-03-11 21:06:19 -0600

    Change the path of AddCompleteWizardActivity class

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddCompleteWizardActivity.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddCompleteWizardActivity.java
@@ -1,9 +1,10 @@
-package ca.ualberta.c301w13t12recipes;
+package ca.ualberta.c301w13t12recipes.view;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import ca.ualberta.c301w13t12recipes.R;
+import ca.ualberta.c301w13t12recipes.R.layout;
 
 public class AddCompleteWizardActivity extends Activity {
 	Button doneButton;
