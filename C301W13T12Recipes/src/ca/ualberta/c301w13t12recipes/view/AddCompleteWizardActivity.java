c6a7ab6463c38550937a61454f0373cb45ab688c
Frank <guanqi@ualberta.ca>
2013-03-12 13:50:02 -0600

    update

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddCompleteWizardActivity.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddCompleteWizardActivity.java
@@ -1,19 +1,63 @@
 package ca.ualberta.c301w13t12recipes.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import ca.ualberta.c301w13t12recipes.R;
 import ca.ualberta.c301w13t12recipes.R.layout;
+import ca.ualberta.c301w13t12recipes.controller.DatabaseController;
+import ca.ualberta.c301w13t12recipes.model.Recipe;
 
 public class AddCompleteWizardActivity extends Activity {
 	Button doneButton;
+	DatabaseController controller;
+	Recipe recipe;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_add_complete_wizard);
-	    // TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_complete_wizard);
+
+		setupWidgets();
+		doneButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				saveRecipe();
+				jumpToMainActivity();
+				
+			}
+
+		});
+
+		// TODO Auto-generated method stub
 	}
 
+	private void setupWidgets() {
+		doneButton = (Button) findViewById(R.id.add_button_complete);
+	}
+
+	private void getRecipe() {
+		recipe = (Recipe) getIntent().getSerializableExtra("NEW_RECIPE");
+
+	}
+
+	private void saveRecipe() {
+		getRecipe();
+		controller = new DatabaseController(AddCompleteWizardActivity.this);
+		controller.addRecipe(recipe);
+
+	}
+
+	private void jumpToMainActivity() {
+		Intent intent = new Intent(AddCompleteWizardActivity.this,MainActivity.class);
+		startActivity(intent);
+
+	}
 }
--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddPicWizardActivity.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddPicWizardActivity.java
@@ -1,57 +1,82 @@
 package ca.ualberta.c301w13t12recipes.view;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import ca.ualberta.c301w13t12recipes.R;
 import ca.ualberta.c301w13t12recipes.model.Recipe;
+
 /**
  * Activity class for adding picture wizard
- *
+ * 
  */
 public class AddPicWizardActivity extends Activity {
 	private Button addButton;
 	private Button nextButton;
-	
+
 	private Recipe recipe;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_add_img_wizard);
-	    
-	    
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_img_wizard);
+		setupWidgets();
+		
+		addButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO adding photos
+				
+			}
+		});
+
+		nextButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO jump to next activity
+				jumpToAddCompleteActivity();
+			}
+		});
 	}
+
 	/**
-	 *	TODO initialize all buttons and imageview widgets
+	 * TODO initialize all buttons and imageview widgets
 	 */
-	private void setupWidgets(){
-		addButton = (Button)findViewById(R.id.add_photo_button_add_photo);
-		addButton = (Button)findViewById(R.id.add_photo_button_next);
+	private void setupWidgets() {
+		addButton = (Button) findViewById(R.id.add_button_complete);
+		nextButton = (Button) findViewById(R.id.add_photo_button_next);
 	}
+
 	/**
 	 * TODO display the sign when the album is empty
 	 */
-	private void showNoImageSign(){
-		
+	private void showNoImageSign() {
+
 	}
+
 	/**
 	 * TODO hide no image sign when the album has at least one photo
 	 */
-	private void hideNoImageSign(){
-		
+	private void hideNoImageSign() {
+
 	}
+
 	/**
-	 * TODO create a new intent that allow 
+	 * TODO create a new intent that allow
 	 */
-	
-	private void getRecipe(){
+
+	private void getRecipe() {
 		recipe = (Recipe) getIntent().getSerializableExtra("NEW_RECIPE");
-		
+
 	}
-	private void jumpToAddCompleteActivity(){
+
+	private void jumpToAddCompleteActivity() {
 		Intent intent = new Intent(AddPicWizardActivity.this,
 				AddCompleteWizardActivity.class);
 		Bundle bundle = new Bundle();
