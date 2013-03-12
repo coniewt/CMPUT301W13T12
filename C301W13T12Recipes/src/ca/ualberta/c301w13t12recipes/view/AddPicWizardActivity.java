6225bfbb3a43b0bee8fdafad90101f01b61079ed
Frank <guanqi@ualberta.ca>
2013-03-12 13:12:36 -0600

    update

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddPicWizardActivity.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/AddPicWizardActivity.java
@@ -2,20 +2,61 @@ package ca.ualberta.c301w13t12recipes.view;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import ca.ualberta.c301w13t12recipes.R;;
+import android.widget.Button;
+import ca.ualberta.c301w13t12recipes.R;
+import ca.ualberta.c301w13t12recipes.model.Recipe;
 /**
  * Activity class for adding picture wizard
  *
  */
 public class AddPicWizardActivity extends Activity {
-
+	private Button addButton;
+	private Button nextButton;
+	
+	private Recipe recipe;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_add_img_wizard);
-	    // TODO Auto-generated method stub
+	    
+	    
 	}
-
+	/**
+	 *	TODO initialize all buttons and imageview widgets
+	 */
+	private void setupWidgets(){
+		addButton = (Button)findViewById(R.id.add_photo_button_add_photo);
+		addButton = (Button)findViewById(R.id.add_photo_button_next);
+	}
+	/**
+	 * TODO display the sign when the album is empty
+	 */
+	private void showNoImageSign(){
+		
+	}
+	/**
+	 * TODO hide no image sign when the album has at least one photo
+	 */
+	private void hideNoImageSign(){
+		
+	}
+	/**
+	 * TODO create a new intent that allow 
+	 */
+	
+	private void getRecipe(){
+		recipe = (Recipe) getIntent().getSerializableExtra("NEW_RECIPE");
+		
+	}
+	private void jumpToAddCompleteActivity(){
+		Intent intent = new Intent(AddPicWizardActivity.this,
+				AddCompleteWizardActivity.class);
+		Bundle bundle = new Bundle();
+		bundle.putSerializable("NEW_RECIPE", recipe);
+		intent.putExtras(bundle);
+		startActivity(intent);
+	}
 }
