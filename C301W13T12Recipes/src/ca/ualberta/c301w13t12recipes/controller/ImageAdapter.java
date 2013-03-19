f7d6a8a2c12af5753efa676f382ea03da630b485
Frank <guanqi@ualberta.ca>
2013-03-19 17:06:15 -0600

    revise

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/controller/ImageAdapter.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/controller/ImageAdapter.java
@@ -1,5 +1,6 @@
 package ca.ualberta.c301w13t12recipes.controller;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import ca.ualberta.c301w13t12recipes.model.Image;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -16,42 +18,42 @@ import android.widget.ImageView;
 public class ImageAdapter extends BaseAdapter{
 	private Context context;
 	private DatabaseController controller;
-	private ArrayList<Image> gallary;
+	private ArrayList<Image> gallery;
 
-	public ImageAdapter(Context newContext,ArrayList<Image> gallary){
+	public ImageAdapter(Context newContext,ArrayList<Image> gallery){
 		// TODO constructor
 		this.context = newContext;	
 		this.controller = new DatabaseController(newContext);
+		this.gallery = gallery;
 	}
 	
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
+	// TODO return size of gallery arraylist
+		return gallery.size();
 	}
 	
-	public void refreshImageLib(){
-		
-		
-		
-		
+	public void refreshImageLib(){	
 	}
 
 	@Override
-	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object getItem(int position) {
+		// TODO get image at a position from gallery
+		return gallery.get(position);
 	}
 
 	@Override
-	public long getItemId(int arg0) {
+	public long getItemId(int position) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
+	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
+		ImageView imageView = new ImageView(context);
+		File image = new File(gallery.get(position).getPath());
+		imageView.setImageURI(Uri.fromFile(image));
 		return null;
 	}
 
