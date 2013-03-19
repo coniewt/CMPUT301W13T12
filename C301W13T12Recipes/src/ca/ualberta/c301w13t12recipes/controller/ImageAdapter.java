166ff0cd67b1780125eec18ece156f198dff85fc
Frank <guanqi@ualberta.ca>
2013-03-19 16:30:24 -0600

    revise

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/controller/ImageAdapter.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/controller/ImageAdapter.java
@@ -1,25 +1,40 @@
 package ca.ualberta.c301w13t12recipes.controller;
 
+import java.util.ArrayList;
+
+import ca.ualberta.c301w13t12recipes.model.Image;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter{
 	private Context context;
+	private DatabaseController controller;
+	private ArrayList<Image> gallary;
 
-
-	public ImageAdapter(Context newContext){
+	public ImageAdapter(Context newContext,ArrayList<Image> gallary){
 		// TODO constructor
-		this.context = newContext;		
+		this.context = newContext;	
+		this.controller = new DatabaseController(newContext);
 	}
+	
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public void refreshImageLib(){
+		
+		
+		
+		
 	}
 
 	@Override
