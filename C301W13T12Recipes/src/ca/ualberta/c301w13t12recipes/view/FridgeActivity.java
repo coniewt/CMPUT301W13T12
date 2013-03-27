9719d7b529848b164cecbcaeb92a9b8c198e8391
Yuwe DUan <yuwei@ualberta.ca>
2013-03-27 11:30:12 -0600

    add two activities

---
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/view/FridgeActivity.java
@@ -0,0 +1,14 @@
+package ca.ualberta.c301w13t12recipes.view;
+
+import ca.ualberta.c301w13t12recipes.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FridgeActivity extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fidge);
+		
+	
+	}
+}
