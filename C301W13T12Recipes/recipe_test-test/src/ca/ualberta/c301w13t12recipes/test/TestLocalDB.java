7b5d6c024a64d912ecc74c22e924e1115d0e2154
Yuwe DUan <yuwei@ualberta.ca>
2013-04-02 12:48:04 -0600

    a little change

--- C301W13T12Recipes/recipe_test-test/src/ca/ualberta/c301w13t12recipes/test/TestLocalDB.java
+++ C301W13T12Recipes/recipe_test-test/src/ca/ualberta/c301w13t12recipes/test/TestLocalDB.java
@@ -7,7 +7,8 @@ import android.test.AndroidTestCase;
  * 
  */
 public class TestLocalDB extends AndroidTestCase {
-	Recipe book1, book1a, book1m, book2;
+	Recipe book1, book1a, book1m, book2,recipe1;
+	LocalDB localdb = new LocalDB(getContext());
 
 	@Override
 	public void setUp() {
@@ -15,8 +16,8 @@ public class TestLocalDB extends AndroidTestCase {
 		book1a = new Recipe("Title1", "Desc 1", "123");
 		book1m = new Recipe("Title1a", "Desc 1a", "dsdf");
 		book2 = new Recipe("Title2", "Desc 2", "sdasd");
+		
 	}
-
 	/**
 	 * To test equal
 	 */
@@ -40,4 +41,14 @@ public class TestLocalDB extends AndroidTestCase {
 		// Books with different isbn are not equal
 		assertFalse(book1.getId().equals(book2.getId()));
 	}
+	/**
+	 * 
+	 */
+	public void testAddrecipe(){
+		localdb.addLocal_Recipe_Table(book1);
+		recipe1 = localdb.get_Local_Recipe(book1.getId());
+		assertSame(book1, recipe1);
+	}
+	
+	
 }
