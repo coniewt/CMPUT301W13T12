3753bbed3cf24dc3bfc946860963d18318e97c91
Yuwe DUan <yuwei@ualberta.ca>
2013-04-02 12:34:46 -0600

    add class to junit

--- C301W13T12Recipes/recipe_test-test/src/ca/ualberta/c301w13t12recipes/test/TestLocalDB.java
+++ C301W13T12Recipes/recipe_test-test/src/ca/ualberta/c301w13t12recipes/test/TestLocalDB.java
@@ -4,8 +4,40 @@ import android.test.AndroidTestCase;
 
 /**
  * @author YUWEi DUAN
- *
+ * 
  */
-public class TestLocalDB extends AndroidTestCase{
+public class TestLocalDB extends AndroidTestCase {
+	Recipe book1, book1a, book1m, book2;
 
+	@Override
+	public void setUp() {
+		book1 = new Recipe("Title1", "Desc 1", "123");
+		book1a = new Recipe("Title1", "Desc 1", "123");
+		book1m = new Recipe("Title1a", "Desc 1a", "dsdf");
+		book2 = new Recipe("Title2", "Desc 2", "sdasd");
+	}
+
+	/**
+	 * To test equal
+	 */
+	public void testEquals() {
+		/* Fixture */
+
+		/*
+		 * Recipe book1 = new Recipe( "Title1", "Desc 1","123"); Recipe book1a =
+		 * new Recipe( "Title1", "Desc 1","123"); Recipe book1m = new Recipe(
+		 * "Title1a", "Desc 1a","dsdf"); Recipe book2 = new Recipe( "Title2",
+		 * "Desc 2","sdasd");
+		 */
+
+		/* Assertions */
+		// A book must be equal to itself
+		assertTrue(book1.equals(book1));
+
+		// Books with the same isbn are consedered equal, even if their titles
+		// and descriptions are different
+		// assertTrue(book1.equals(book1m));
+		// Books with different isbn are not equal
+		assertFalse(book1.getId().equals(book2.getId()));
+	}
 }
