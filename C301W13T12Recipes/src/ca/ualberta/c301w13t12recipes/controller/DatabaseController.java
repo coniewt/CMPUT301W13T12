c5d58d8d4eb579ee43ce89f916fa9e40690b2e93
coniewt <coniewt@hotmail.com>
2013-04-02 08:07:36 -0600

    javadoc

--- C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/controller/DatabaseController.java
+++ C301W13T12Recipes/src/ca/ualberta/c301w13t12recipes/controller/DatabaseController.java
@@ -18,7 +18,7 @@ import ca.ualberta.c301w13t12recipes.model.Recipe;
 import ca.ualberta.c301w13t12recipes.model.StrResource;
 
 /**
- * Database controller class, used for various background handling purpose.
+ * Database controller class, used for background database handling.
  * 
  * @author GUANQI HUANG
  * 
@@ -29,33 +29,36 @@ public class DatabaseController {
 	private Context context;
 
 	/**
-	 * @param context
+	 * Constructor
+	 * @param Context
 	 */
 	public DatabaseController(Context context) {
 		this.database = new LocalDB(context);
 		this.context = context;
 	}
+	
 	/**
-	 * Get local database
+	 * Get local database instance
 	 * 
-	 * @return localDB object
+	 * @return LocalDB - Current instance of the database
 	 */
 	public LocalDB getDB() {
 		return this.database;
 	}
 
 	/**
-	 * Add recipe into local database
+	 * Add a recipe into local database
 	 * 
-	 * @param recipe
-	 *            object
+	 * @param Recipe A recipe object
 	 */
 	public void addRecipe(Recipe re) {
 		this.database.addLocal_Recipe_Table(re);
 	}
 
 	/**
-	 * remove all recipe in local database
+	 * Delete a recipe from the local database
+	 * 
+	 * @param Recipe A recipe object
 	 */
 	public void removeAll() {
 		this.database.clear_All();
@@ -69,40 +72,45 @@ public class DatabaseController {
 	}
 
 	/**
-	 * @param key
-	 *            a string of keyword
-	 * @return list<recipe>
+	 * @param String A string of keyword for searching
+	 * 
+	 * @return List<Recipe> - A list of Recipe object
 	 */
 	public List<Recipe> search(String key) {
 		return this.database.searchRecipebyKeyword(key);
 	}
 
 	/**
-	 * @return the name list
+	 * Pop up suggest search keyword for auto complete functionality
+	 * 
+	 * @return List<String> - A list of suggested keywords
 	 */
 	public List<String> getNameList() {
 		return this.database.getAutoCompleteKeyword();
 	}
 
 	/**
-	 * @param recipe
-	 * @return if the recipe is in local database,return true, else return false
+	 * Check if the recipe is existed locally
+	 * @param Recipe A recipe object
+	 * @return boolean - Return True if recipe is already existed locally, False if not
 	 */
 	public boolean isLocalRecipeExists(Recipe recipe) {
 		return this.database.isLocalIdExists(recipe);
 	}
 
 	/**
-	 * @param recipe
-	 * @return if the recipe is in remote database,return true, else return
-	 *         false
+	 * Check if the recipe is existed on web server
+	 * @param Recipe A recipe object
+	 * @return boolean - Return True if recipe is already existed on web server, False if not
 	 */
 	public boolean isRemoteRecipeExists(Recipe recipe) {
 		return this.database.isRemoteIdExists(recipe);
 	}
 
 	/**
-	 * @param recipe
+	 * Post recipe to web server
+	 * 
+	 * @param Recipe A recipe object
 	 */
 	public void postRemote(List<Recipe> li) {
 		for (Recipe re : li) {
@@ -114,7 +122,7 @@ public class DatabaseController {
 	/**
 	 * Get a list of ingredients from local ingredients database
 	 * 
-	 * @return ArrayList<Ingredient>
+	 * @return ArrayList<Ingredient> - A list of ingredient
 	 */
 	public ArrayList<Ingredient> getIngredListFromIngredDB() {
 		return this.database.getLocal_Ingredient_List();
@@ -122,16 +130,18 @@ public class DatabaseController {
 	}
 
 	/**
-	 * Remove the item at specific position
-	 * @param ingredient the position of database
+	 * Remove an ingredient from a recipe in local database
+	 * 
+	 * @param Ingredient An ingredient object
 	 */
 	public void removeIngredFromIngredDB(Ingredient ingredient) {
 		this.database.removeLocal_Ingredient_List(ingredient);// this.database.
 	}
 
 	/**
-	 * Add the ingredients
-	 * @param in the ingredient object
+	 * Add an ingredient to a recipe in local database
+	 * 
+	 * @param Ingredient An ingredient object
 	 */
 	public void addIngredFromIngredDB(Ingredient in) {
 		this.database.addLocal_Ingredient_Table(in);
